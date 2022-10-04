4da3a750e0f78649723c08c26c6e6fd55bbd33bd
Chathura Dilushanka <90453201+demo-cracker@users.noreply.github.com>
2022-10-04 07:40:11 +0530

    summary

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 getsubs();
 async function getsubs(){
 
-  // HelaLK
+  // Use You google cloud console Yt api after "&key="
 
     const channelIdCarry = 'UCm1_-vkonHv3nQuvux5xtDw'
     const urlCarry = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelIdCarry}&key=AIzaSyCjlpCxj8Xh5Qh_yhTV3KAlyo7EH3oPCV8`;
@@ -12,6 +12,7 @@ async function getsubs(){
     const subscriberCountCarry = dataCarry.items[0].statistics.subscriberCount
     console.log(subscriberCountCarry);
     const rootCarry = document.getElementById('carry');
+    // You can change d2020d for place you need to display sub count
     const carry = document.createElement('d2020d');
     const nodeCarry = document.createTextNode(`Subscriber : ${subscriberCountCarry}`);
     carry.append(nodeCarry);
